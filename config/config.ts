571904f2a865e3ed46c9548da87fc98355a69bc3
bijinfeng <bijinfeng@bytedance.com>
2022-09-29 18:45:32 +0800

    fix(tabler-ui): fix dumi config alias error

--- config/config.ts
+++ config/config.ts
@@ -8,10 +8,11 @@ export default defineConfig({
     includes: ['docs', 'packages/tabler-react']
   },
   alias: {
-    '@kebai/tabler-react': "/packages/tabler-react/src",
-    "@kebai/tabler-ui": "/packages/tabler-ui",
+    '@kebai/tabler-react': process.cwd() + "/packages/tabler-react/src",
+    "@kebai/tabler-ui": process.cwd() + "/packages/tabler-ui",
   },
   dynamicImportSyntax: {},
+  ignoreMomentLocale: true,
   outputPath: "docs-dist",
   base: process.env.NODE_ENV === 'production' ? '/tabler-ui/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/tabler-ui/' : '/',
